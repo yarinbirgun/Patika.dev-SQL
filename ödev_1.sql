76873f5586731b8ba714524bc4d9f69836f50059
Talha Mücahit ÖZBEK <86835874+yarinbirgun@users.noreply.github.com>
2021-07-06 23:47:25 +0300

    Create ödev_1.sql

---
+++ ödev_1.sql
@@ -0,0 +1,12 @@
+1.SELECT title , description FROM film
+2.WHERE length > 60 AND length < 75 AND
+
+
+
+3.SELECT * FROM film
+ WHERE rental_rate = 0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99
+
+4.Smith
+
+SELECT * FROM customer
+WHERE first_name ='Mary';
